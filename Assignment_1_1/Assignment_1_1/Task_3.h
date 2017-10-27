a54c88b816c032bc8f46fd8a40b65501f604743a
SophieHegarty <shegarty874@gmail.com>
2017-10-27 02:04:52 +0100

    Assignment 1 of Data structures and algorithms

---
+++ Assignment_1_1/Assignment_1_1/Task_3.h
@@ -0,0 +1,14 @@
+//
+//  Task_3.h
+//  Assignment_1_1
+//
+//  Created by Sophie Hegarty on 26/10/2017.
+//  Copyright © 2017 Sophie Hegarty. All rights reserved.
+//
+
+#ifndef Task_3_h
+#define Task_3_h
+
+#include <stdio.h>
+
+#endif /* Task_3_h */
